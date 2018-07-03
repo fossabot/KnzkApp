f098700c2bc06fe447f0af8f0be2cf012d8bbfbb
yuzulabo <git-yuzu@svk.jp>
2018-07-03 22:54:39 +0900

    wip

--- www/js/dev_mode.js
+++ www/js/dev_mode.js
@@ -1,37 +1,32 @@
 function OpenDevMode() {
-  ons.openActionSheet({
-    title: 'デベロッパーモード',
-    cancelable: true,
-    buttons: [
-      'システム情報/ログを表示',
-      {
-        label: '設定データを編集',
-        modifier: 'destructive'
-      },
-      {
-        label: i18next.t('navigation.cancel'),
-        icon: 'md-close'
+  ons
+    .openActionSheet({
+      title: 'デベロッパーモード',
+      cancelable: true,
+      buttons: [
+        'システム情報/ログを表示',
+        {
+          label: i18next.t('navigation.cancel'),
+          icon: 'md-close',
+        },
+      ],
+    })
+    .then(function(index) {
+      if (index === 0) DevModeLog();
+      else if (index === 999) {
+        //WIP
+        ons.notification
+          .confirm(i18next.t('bookmark.clear1.text'), { title: 'CAUION' })
+          .then(function(e) {
+            if (e === 1) {
+              var bookmark = loadBookmark();
+              bookmark[inst] = [];
+              saveBookmark(bookmark);
+              BackTab();
+            }
+          });
       }
-    ]
-  }).then(function (index) {
-    if (index === 0) DevModeLog();
-    else if (index === 1) {
-      ons.notification
-        .confirm(
-          i18next.t('bookmark.clear1.text', {
-            inst: inst,
-            interpolation: { escapeValue: false },
-          }),
-          { title: 'CAUION' }
-        )
-        .then(function (e) {
-          if (e === 1) {
-            var bookmark = loadBookmark();
-            bookmark[inst] = [];
-            saveBookmark(bookmark);
-            BackTab();
-          }
-        });
-    }
-  });
+    });
 }
+
+function DevModeLog() {}
